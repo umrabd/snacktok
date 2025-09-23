24dd08f59bd74040f91677c01a1681f5d28ff532
Umer Abdullah <umrabd@gmail.com>
2025-09-23 16:09:36 +0500

    fixes

--- backend/src/routes/food.routes.js
+++ backend/src/routes/food.routes.js
@@ -1,26 +1,21 @@
 const express = require("express");
 const router = express.Router();
-const {
-  getAllFoods,
-  getFoodById,
-  createFood,
-  updateFood,
-  deleteFood
-} = require("../controller/food.controller");
+const foodController = require("../controller/food.controller");
+const authMiddleware = require("../middlewares/auth.middleware")
 
 // GET /api/foods - Get all food items
-router.get('/', getAllFoods);
+router.get('/', foodController.getAllFoods);
 
 // GET /api/foods/:id - Get a specific food item
-router.get('/:id', getFoodById);
+router.get('/:id', foodController.getFoodById);
 
 // POST /api/foods - Create a new food item
-router.post('/', createFood);
+router.post('/', authMiddleware.authFoodPartnerMiddleware, foodController.createFood);
 
 // PUT /api/foods/:id - Update a food item
-router.put('/:id', updateFood);
+router.put('/:id', foodController.updateFood);
 
 // DELETE /api/foods/:id - Delete a food item
-router.delete('/:id', deleteFood);
+router.delete('/:id', foodController.deleteFood);
 
 module.exports = router;
